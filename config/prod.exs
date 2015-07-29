8b772dbf71446d357d8bb0dc4bc49b90b240b908
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-07-30 07:07:46 +0900

    add some configurations

--- config/prod.exs
+++ config/prod.exs
@@ -15,6 +15,7 @@ config :web_qa, WebQa.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # ## SSL Support
 #
@@ -46,7 +47,8 @@ config :logger, level: :info
 #
 #     config :web_qa, WebQa.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+# Configure your database
+config :web_qa, WebQa.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL")
+  size: 20 # The amount of database connections in the pool
