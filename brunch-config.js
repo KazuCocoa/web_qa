b232de212c474e05ec2c22c37881afa41d8a507b
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-05-14 22:29:19 +0900

    add hiding table

--- brunch-config.js
+++ brunch-config.js
@@ -20,8 +20,7 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: 'css/app.css',
-      joinTo: 'css/materialize.css'
+      joinTo: 'css/app.css'
     },
     templates: {
       joinTo: 'js/app.js'
